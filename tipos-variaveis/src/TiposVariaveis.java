6dd043f380cbd303857f60fa3836267e2e88d91d
Jullyana Breciani <jullyanabadami@gmail.com>
2024-05-09 22:17:31 -0300

    09/05

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2.500;
+
+        // Necessário se atentar aos tipos e seus limites.
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+
+        // Não pode ser mudada. 
+        final double VALOR_PI = 3.14;
+
     }
 }
